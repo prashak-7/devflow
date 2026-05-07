edc06a72b5ba1b21d69c697925150daceb3c5f1f
prashak-7 <prashantpathak175@gmail.com>
2026-05-07 18:40:01 +0545

    Implement ESLint and Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,7 +7,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -27,7 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,67 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import { flatConfigs } from "eslint-plugin-import-x";
+import neostandard from "neostandard";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  ...neostandard({
+    noStyle: true,
+    ts: true,
+  }),
+
+  flatConfigs.recommended,
+  flatConfigs.typescript,
+
+  {
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+        }),
+      ],
+    },
+    rules: {
+      "import-x/no-unresolved": "error",
+      "import-x/no-duplicates": "error",
+      "import-x/no-cycle": "warn",
+      "import-x/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "never",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "import-x/first": "error",
+    },
+  },
+
+  prettier,
+
+  {
+    rules: {
+      "no-console": "warn",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
-
-export default eslintConfig;
